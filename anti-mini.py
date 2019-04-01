0267b973a044182f616c1a6b69d45468a0b445fc
yinmo-public <yinmo.contact@gmail.com>
2019-04-01 14:49:23 +0800

    Add files via upload

---
+++ anti-mini.py
@@ -0,0 +1,41 @@
+# -*-coding: utf-8 -*-
+from linepy import *
+from datetime import datetime
+from time import sleep
+#==============================================================================#
+me = LINE()
+meMID = me.profile.mid
+oepoll = OEPoll(me)
+Me = [meMID,"u6949f816dddef051b5762322e5597f1e"]
+#==============================================================================#
+def logError(text):
+    me.log("[ 錯誤 ] " + str(text))
+#==============================================================================#
+def lineBot(op):
+    try:
+        if op.type == 0:
+            return
+        if op.type == 5:
+            me.blockContact(op.param1)
+        if op.type == 6:
+            contact = me.getContact(op.param1)
+            print ("[ 6 ] {} 試圖騷擾您 已被系統封鎖".format(contact.displayName))
+        if op.type == 21 or op.type == 22 or op.type ==24:
+            print ("[ 通知離開副本 ]")
+            me.leaveRoom(op.param1)
+    except Exception as error:
+        print(error)
+#==============================================================================#
+while 1:
+    try:
+        ops = oepoll.singleTrace(count=50)
+        if ops is not None:
+            for op in ops:
+                lineBot(op)
+                oepoll.setRevision(op.revision)
+                thread = threading.Thread(target=bot, args=(op,))
+                thread.start()
+    except:
+        pass
+
+
