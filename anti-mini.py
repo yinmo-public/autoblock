2495d49ee4dd314fcbace02c57055fe18a8dd6a1
yinmo-public <yinmo.contact@gmail.com>
2019-04-10 19:39:17 +0800

    Update anti-mini.py

--- anti-mini.py
+++ anti-mini.py
@@ -12,15 +12,9 @@ def logError(text):
 #==============================================================================#
 def lineBot(op):
     try:
-        if op.type == 0:
-            return
         if op.type == 5:
             me.blockContact(op.param1)
-        if op.type == 6:
-            contact = me.getContact(op.param1)
-            print ("[ 6 ] {} 試圖騷擾您 已被系統封鎖".format(contact.displayName))
         if op.type == 21 or op.type == 22 or op.type ==24:
-            print ("[ 通知離開副本 ]")
             me.leaveRoom(op.param1)
     except Exception as error:
         print(error)
